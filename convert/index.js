2cb3e842d01b00324af4d198a1ed3737c06b63b6
Mohmed Bamhraz <mohmedbamhrz@gmail.com>
2017-11-18 19:15:43 -0800

    - update index.js  - update actions/index.js

--- convert/index.js
+++ convert/index.js
@@ -1,7 +1,7 @@
 const electron = require('electron')
 const ffmepg = require('fluent-ffmpeg')
 const _ = require('lodash')
-const { app, BrowserWindow, ipcMain } = electron
+const { app, BrowserWindow, ipcMain, webContents, shell } = electron
 
 let mainWindow;
 
@@ -16,26 +16,40 @@ app.on('ready', () => {
 })
 
 ipcMain.on('videos:added', ( event, videos ) => {
-  // const promise = new Promise ( (resolve, reject) => {
-  //   ffmepg.ffprobe( videos[0].path, (err, metadata) => {
-  //     resolve(metadata)
-  //   })
-  // })
-
-  // promise.then((metadata) => { console.log(metadata) })
-
   const promises = _.map(videos, video => {
     return new Promise((resolve, reject) => {
       ffmepg.ffprobe(video.path, (err, metadata)=> {
-        resolve({
-          ...video,
-          duration: metadata.format.duration,
-          format: 'avi'
-        })
+        video.duration = metadata.format.duration;
+        video.format = 'avi'
+        resolve(video)
       })
     })
   })
 
   Promise.all(promises)
-    .then((results) => console.log(results))
+    .then((results) => {
+      mainWindow.webContents.send('metadata:complete', results)
+    })
 })  
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name)[0]
+    const outputName = video.name.split('.')[0]
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`
+    // console.log(outputPath)
+    ffmepg(video.path)
+    .output(outputPath)
+    .on('progress', ({ timemark }) => 
+      mainWindow.webContents.send('conversion:progress', {video , timemark })
+    )
+    .on('end',() => 
+      mainWindow.webContents.send('conversion:end', { video , outputPath })
+    )
+    .run()
+  })
+})
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath);
+});
